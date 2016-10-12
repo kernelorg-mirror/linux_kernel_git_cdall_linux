12010a93fda9eaa26549aa4dc2c80d258dcce6c6
Christoffer Dall <christoffer.dall@linaro.org>
2016-10-12 22:09:24 +0200

    arm64: Use physical counter for in-kernel reads
    Using the physical counter allows KVM to retain the offset between the
    virtual and physical counter as long as it is actively running a VCPU.
    
    As soon as a VCPU is released, another thread is scheduled or we start
    running userspace applications, we reset the offset to 0, so that VDSO
    operations can still read the virtual counter and get the same view of
    time as the kernel.
    
    This opens up potential improvements for KVM performance.
    
    VHE kernels or kernels using the virtual timer are unaffected by this.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -597,7 +597,7 @@ static void __init arch_counter_register(unsigned type)
 
 	/* Register the CP15 based counter if we have one */
 	if (type & ARCH_CP15_TIMER) {
-		if (IS_ENABLED(CONFIG_ARM64) || arch_timer_uses_ppi == VIRT_PPI)
+		if (arch_timer_uses_ppi == VIRT_PPI || is_kernel_in_hyp_mode())
 			arch_timer_read_counter = arch_counter_get_cntvct;
 		else
 			arch_timer_read_counter = arch_counter_get_cntpct;
