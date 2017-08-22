b0fa2e8ee3751af06ef392bcfd4157e66f7526fc
Jintack Lim <jintack.lim@linaro.org>
2017-08-22 11:22:01 -0500

    KVM: arm/arm64: Manage mmus for nested VMs
    Now that a hypervisor can run in the virtual EL2, the guest hypervisor
    can assign any VMID to its own VMs. To avoid conflicts between VMIDs
    among a host and guest(s), the host hypervisor maps each VMID from a
    guest hypervisor's view (i.e. virtual VMID) to an unique shadow VMID.
    It also manages a set of shadow stage-2 page tables for each shadow
    VMID. All this information is stored in kvm_nested_s2_mmu struct.
    
    A host hypervisor manages a list of kvm_nested_s2_mmu objects per VM. On
    a VM entry it searches an object in the list using a virtual VMID as a
    key.
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

--- arch/arm64/include/asm/kvm_emulate.h
+++ arch/arm64/include/asm/kvm_emulate.h
@@ -361,6 +361,11 @@ static inline bool vcpu_el2_format_used(const struct kvm_vcpu *vcpu)
 		(__vcpu_sys_reg(vcpu, HCR_EL2) & HCR_NV1));
 }
 
+static inline bool vcpu_nested_stage2_enabled(const struct kvm_vcpu *vcpu)
+{
+	return (vcpu_read_sys_reg(vcpu, HCR_EL2) & HCR_VM);
+}
+
 static inline u32 kvm_vcpu_get_hsr(const struct kvm_vcpu *vcpu)
 {
 	return vcpu->arch.fault.esr_el2;
@@ -567,14 +572,6 @@ static inline unsigned long vcpu_data_host_to_guest(struct kvm_vcpu *vcpu,
 	}
 
 	return data;		/* Leave LE untouched */
-}
-
-static inline struct kvm_s2_vmid *vcpu_get_active_vmid(struct kvm_vcpu *vcpu)
-{
-	if (unlikely(is_hyp_ctxt(vcpu)))
-		return &vcpu->kvm->arch.mmu.el2_vmid;
-
-	return &vcpu->kvm->arch.mmu.vmid;
 }
 
 #endif /* __ARM64_KVM_EMULATE_H__ */
