0ee4c030d4a9653191f78c11ba2092bdd9c7b9be
Christoffer Dall <christoffer.dall@linaro.org>
2017-08-22 03:02:42 -0500

    KVM: arm/arm64: Forward the guest hypervisor's stage 2 permission faults
    We can have discrepancies between the nested stage 2 page table and the
    shadow one in a couple of cases.  For example, the guest hypervisor can
    mark a page writable but the host hypervisor maps the page read-only in
    the shadow page table, if using something like KSM on the host level.
    In this case, a write fault is handled directly by the host hypervisor.
    But we could also simply have a read-only page mapped read-only in both
    tables, in which case the host hypervisor cannot do anything else than
    telling the guest hypervisor about the fault.
    
    If taking an instruction abort, we need to injected a nested fault if
    the L1 guest doesn't have cleared that bit in its S2 page tables.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -545,6 +545,8 @@ struct kvm_s2_mmu *vcpu_get_active_s2_mmu(struct kvm_vcpu *vcpu);
 void update_nested_s2_mmu(struct kvm_vcpu *vcpu);
 int kvm_walk_nested_s2(struct kvm_vcpu *vcpu, phys_addr_t gipa,
 		       struct kvm_s2_trans *result);
+int kvm_s2_handle_perm_fault(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
+			     struct kvm_s2_trans *trans);
 int kvm_inject_s2_fault(struct kvm_vcpu *vcpu, u64 esr_el2);
 
 static __always_inline u64 kvm_get_vttbr(struct kvm_s2_mmu *mmu)
