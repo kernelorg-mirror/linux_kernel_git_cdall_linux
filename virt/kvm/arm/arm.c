16af682d1e006a507c5be90ab04d4b1a06fe5a71
Christoffer Dall <christoffer.dall@linaro.org>
2017-10-03 14:02:12 +0200

    KVM: arm64: Introduce VHE-specific kvm_vcpu_run
    So far this is just a copy of the legacy non-VHE switch function, where
    we only change the existing calls to has_vhe() in both the original and
    new functions.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/arm/arm.c
+++ virt/kvm/arm/arm.c
@@ -712,7 +712,10 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu, struct kvm_run *run)
 		trace_kvm_entry(*vcpu_pc(vcpu));
 		guest_enter_irqoff();
 
-		ret = kvm_call_hyp(__kvm_vcpu_run, vcpu);
+		if (has_vhe())
+			ret = kvm_vcpu_run_vhe(vcpu);
+		else
+			ret = kvm_call_hyp(__kvm_vcpu_run_nvhe, vcpu);
 
 		vcpu->mode = OUTSIDE_GUEST_MODE;
 		vcpu->stat.exits++;
