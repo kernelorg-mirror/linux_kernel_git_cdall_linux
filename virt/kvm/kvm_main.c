e7de28077f328493bcdfabd1e3e30e9584a21140
Christoffer Dall <christoffer.dall@linaro.org>
2017-11-25 15:52:51 +0100

    KVM: Move vcpu_load to arch-specific kvm_arch_vcpu_ioctl_get_mpstate
    Move vcpu_load() and vcpu_put() into the architecture specific
    implementations of kvm_arch_vcpu_ioctl_get_mpstate().
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2611,11 +2611,7 @@ out_free1:
 	case KVM_GET_MP_STATE: {
 		struct kvm_mp_state mp_state;
 
-		r = vcpu_load(vcpu);
-		if (r)
-			goto out;
 		r = kvm_arch_vcpu_ioctl_get_mpstate(vcpu, &mp_state);
-		vcpu_put(vcpu);
 		if (r)
 			goto out;
 		r = -EFAULT;
