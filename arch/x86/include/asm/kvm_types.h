70c3f96091045c95a87a3b02f2dea182d979ca35
Christoffer Dall <christoffer.dall@arm.com>
2019-01-27 13:26:38 -0800

    KVM: x86: Move mmu_memory_cache functions to common code
    We are currently duplicating the mmu memory cache functionality quite
    heavily between the architectures that support KVM.  As a first step,
    move the x86 implementation (which seems to have the most recently
    maintained version of the mmu memory cache) to common code.
    
    We introduce an arch-specific kvm_types.h which can be used to specify
    how many objects are required in the memory cache, an aspect which
    diverges across architectures.  Since kvm_host.h defines structures with
    fields of the memcache object, we define the memcache structure in
    kvm_types.h, and we include the architecture-specific kvm_types.h to
    know the size of object in kvm_host.h.  We only include asm/kvm_types.h
    if CONFIG_HAVE_KVM is defined, since otherwise the header check compile
    will fail for include/kvm_types.h on non-KVM architectures.
    
    We only define the functions and data types if
    KVM_ARCH_WANT_MMU_MEMORY_CACHE is defined, because not all architectures
    require the mmu memory cache.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>

---
+++ arch/x86/include/asm/kvm_types.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_KVM_TYPES_H
+#define _ASM_X86_KVM_TYPES_H
+
+#define KVM_ARCH_WANT_MMU_MEMORY_CACHE
+
+#define KVM_NR_MEM_OBJS 40
+
+#endif /* _ASM_X86_KVM_TYPES_H */
